a6938872f12cdeaabbb70a08d4250845a47acd41
abhibnp <64185535+abhibnp@users.noreply.github.com>
2020-11-07 14:43:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+/*var React = require("react");
+var ReactDom = require("react-dom");
+*/
+ReactDom.render(
+  <h1>Hello Abhishek!! its your 1st React application</h1>,
+  document.getElementById("root")
+);
